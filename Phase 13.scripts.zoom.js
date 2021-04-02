f1b5c45ddfdf6da56b7b447c7f3a7417ff96bb01
Ghost <somemail@mail.com>
2021-04-02 23:20:36 +0200

    Zoom no longer affects mobile devices, overlay text extended

--- Phase 13.scripts.zoom.js
+++ Phase 13.scripts.zoom.js
@@ -1,1 +1,2 @@
-$("body").css("zoom", 1 / window.devicePixelRatio)
+if (window.matchMedia("only screen and (min-device-width: 768px)").matches)
+    $("body").css("zoom", 1 / window.devicePixelRatio)
